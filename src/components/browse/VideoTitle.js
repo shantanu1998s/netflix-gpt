03bfb3137c1ed0b81ae5eed4173078d1ffe0180e
Shantanu <shantanuprasad55@gmail.com>
2024-03-08 08:37:48 +0530

    jj

--- src/components/browse/VideoTitle.js
+++ src/components/browse/VideoTitle.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className=" pt-36 px-12 absolute text-white">
+    <div className=" pt-24 px-12 absolute text-white">
       <h1 className="font-bold text-6xl">{title}</h1>
       <p className="py-6 text-lg w-1/4" >{overview}</p>
       <div>
